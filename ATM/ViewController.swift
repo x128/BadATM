2998d44b718cde409e64dcf3b9f4b180b65a0ec4
Dmitry Isaev <d.isaev@koronapay.eu>
2022-11-14 09:19:38 +0200

    Initial Commit

---
+++ ATM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATM
+//
+//  Created by Dmitry Isaev on 14/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
